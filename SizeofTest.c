fae39b96035dcec32b2b9a52c210eb6cd038e364
Tim Song <timmy_song@163.com>
2017-09-08 11:48:23 +0800

    Use array variable like a pointer in C!

--- SizeofTest.c
+++ SizeofTest.c
@@ -11,6 +11,9 @@ void msg_size(char msg[]){
 int main() {
   char msg[] = "Turtles!"; /* Size should be 9: 8 characters plus a \0 end character.*/
   printf("[%s] occupies %lu bytes\n", msg, sizeof(msg)); /* size value 9 */
+  printf("The msg variable value is stored at: %p\n", msg);
+  /* We pass the msg directly here, compiler did not complain about it,
+  that means we can use array variable like a pointer.*/
   puts("\nCall msg_size function:\n");
   msg_size(msg); /* size value 8 */
   return 0;
