70143dd80da05621ca183df3d37a0297848c2f4d
Tim Song <timmy_song@163.com>
2017-09-08 14:13:07 +0800

    Find out how C function treat array parameter like pointer.

--- SizeofTest.c
+++ SizeofTest.c
@@ -2,14 +2,16 @@
 
 #include <stdio.h>
 
+/* msg parameter is actually a pointer variable. */
 void msg_size(char msg[]){
-  printf("[%s] occupies %lu bytes\n", msg, sizeof(msg));
+  printf("msg_size read content of msg: %s\n", msg); /* msg is a pointer to a character value's address, the target type - character - is matched with the format s. So it worked out well. */
+  printf("[%s] occupies %lu bytes\n", msg, sizeof(msg)); /* sizeof(msg) is returning the size of a pointer to (start character of) a string here. */
   /* sizeof on array function parameter will return size of
   'char *' instead of 'char []' -- Wsizeof-array-argument */
 }
 
 int main() {
-  char msg[] = "Turtles!"; /* Size should be 9: 8 characters plus a \0 end character.*/
+  char msg[] = "Cookies make you fat!"; /* Size should be 9: 8 characters plus a \0 end character.*/
   printf("[%s] occupies %lu bytes\n", msg, sizeof(msg)); /* size value 9 */
   printf("The msg variable value is stored at: %p\n", msg);
   /* We pass the msg directly here, compiler did not complain about it,
