173d79388f9354b84c45941d38de5d87488fdff5
Tim Song <timmy_song@163.com>
2017-09-11 17:36:19 +0800

    Explore how memory works when you set an array to a constant(such as string literal).

--- ThreeCardMonte.c
+++ ThreeCardMonte.c
@@ -2,7 +2,12 @@
 
 /* A variable points to a string literal can't be used to change the contents of the string. */
 int main(){
-  char *cards = "JQK"; /* Note: cards here is a pointer variable, not an array variable. */
+  char cards[] = "JQK"; /* Note: cards here is an array variable.
+  The computer loads "JQK" into constant memory blck (read-only),
+  then it initialize the array variable into stack memory block
+  and copy the content into the brand new array. So you can use
+  the array variable cards to modify its own content as much as you like.
+   */
   char a_card = cards[2]; /* a_card: K*/
   cards[2] = cards[1]; /* JQK --> JQQ */
   cards[1] = cards[0]; /* JQQ --> JJQ */
