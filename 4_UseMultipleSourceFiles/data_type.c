2f7a4f626b34d6725826fa9d24abc9efce14351c
Tim Song <timmy_song@163.com>
2017-09-19 08:57:23 +0800

    Explore different sizes of data types in C.

---
+++ 4_UseMultipleSourceFiles/data_type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  puts("Explore data type sizes in unit of byte:");
+  printf("* sizeof char: %lu\n", sizeof(char));
+  printf("* sizeof int: %lu\n", sizeof(int));
+  printf("* sizeof short: %lu\n", sizeof(short));
+  printf("* sizeof long: %lu\n", sizeof(long));
+
+  return 0;
+}
