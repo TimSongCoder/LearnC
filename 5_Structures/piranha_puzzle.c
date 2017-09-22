610a8747eada6505bbf1228feec215a380974b3c
Tim Song <timmy_song@163.com>
2017-09-22 16:12:46 +0800

    Nesting struct to model complicated data.

--- 5_Structures/piranha_puzzle.c
+++ 5_Structures/piranha_puzzle.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 
+struct exercise {
+  const char *description;
+  float duration;
+};
+
+struct meal {
+  const char *ingredients;
+  float weight;
+};
+
+struct preferences {
+  struct meal food;
+  struct exercise exercise;
+};
+
 struct fish {
   const char *name;
   const char *species;
   int teeth;
   int age;
-  float weight;
+  struct preferences care;
 };
 
 void catalog(struct fish f){
   printf("%s is a %s with %i teeth. He is %i\n",
           f.name, f.species, f.teeth, f.age);
-}
+};
 
 void label(struct fish f){
-  printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\nWeight:%.1fKg\n",
-          f.name, f.species, f.age, f.teeth, f.weight);
+  printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\nFeed with %2.2f lbs of %s and allow to %s for %2.2f hours\n",
+          f.name, f.species, f.age, f.teeth,
+          f.care.food.weight, f.care.food.ingredients,
+          f.care.exercise.description, f.care.exercise.duration);
 }
 
 int main(){
-  struct fish snappy = {"Snappy", "Piranha", 69, 4, 1.5};
+  struct fish snappy = {"Snappy", "Piranha", 69, 4, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
   catalog(snappy);
   puts("***************************************");
   label(snappy);
