9893dfd935865fba8b919616be5e75c70181940e
Tim Song <timmy_song@163.com>
2017-09-25 08:25:32 +0800

    Use a more readable struct pointer notation, leave the parentheses stuff.

--- 5_Structures/turtle_grow.c
+++ 5_Structures/turtle_grow.c
@@ -7,9 +7,9 @@ typedef struct {
 } turtle;
 
 void happy_birthday(turtle *t){
-  (*t).age = (*t).age + 1;
+  t->age = t->age + 1;
   printf("Happy Birthday %s! You are now %i years old!\n",
-          (*t).name, (*t).age);
+          t->name, t->age);
 }
 
 int main(){
