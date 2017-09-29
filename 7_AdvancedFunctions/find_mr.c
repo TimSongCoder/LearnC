78504f6cd0e98f0b42f0bf4bcfacca8d36a12c4b
Tim Song <timmy_song@163.com>
2017-09-29 11:13:00 +0800

    Use function pointers to stretch the find_mr program flexibility.

--- 7_AdvancedFunctions/find_mr.c
+++ 7_AdvancedFunctions/find_mr.c
@@ -13,14 +13,30 @@ char *ADS[] = {
   "Jed: DBM likes theater, books and dining"
 };
 
-void find(){
+int sports_no_bieber(char *s){
+  return strstr(s, "sports") && !strstr(s, "bieber");
+}
+
+int sports_or_workout(char *s){
+  return strstr(s, "sports") || strstr(s, "working out");
+}
+
+int ns_theater(char *s){
+  return strstr(s, "NS") && strstr(s, "theater");
+}
+
+int arts_theater_or_dining(char *s){
+  return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
+/* Add a function pointer parameter to make the find function versatile. */
+void find(int (*match)(char*)){
   int i;
   puts("Search results:");
   puts("------------------------------------");
 
   for(i = 0; i < NUM_ADS; i++){
-    if(strstr(ADS[i], "sports")
-      && !strstr(ADS[i], "bieber")){
+    if(match(ADS[i])){
       printf("%s\n", ADS[i]);
     }
   }
@@ -29,6 +45,14 @@ void find(){
 }
 
 int main(){
-  find();
+  /* A function name is actually a pointer to the piece of code. */
+  find(sports_no_bieber);
+
+  find(sports_or_workout);
+
+  find(ns_theater);
+
+  find(arts_theater_or_dining);
+  
   return 0;
 }
