a9888d9fb5f5a75f14190ba2a3cd3fe82416441e
Tim Song <timmy_song@163.com>
2017-11-02 10:36:07 +0800

    Use mutex to manage threads traffic.

--- 12_Threads/beer_counter.c
+++ 12_Threads/beer_counter.c
@@ -6,11 +6,34 @@
 
 int beers = 2000000;
 
+/* PTHREAD_MUTEX_INITIALIZER macro initializes a static mutex, setting its attributes
+to default values, no error checking is performed. Defined in pthread.h header file. */
+pthread_mutex_t beers_lock = PTHREAD_MUTEX_INITIALIZER;
+
 void * drink_lots(void *a){
   int i;
+  /* pthread_mutex_lock -- lock a mutex. <pthread.h>
+  int pthread_mutex_lock(pthread_mutex_t *mutex);
+
+  The pthread_mutex_lock() function locks 'mutex'. If the mutex is already locked,
+  the calling thread will block until the mutex becomes available.
+  If successful, the pthread_mutex_lock() function will return zero, otherwise
+  an error number will be returned to indicate the error. */
+  pthread_mutex_lock(&beers_lock);
   for (i = 0; i < 100000; i++){
    beers = beers - 1;
   }
+  /* pthread_mutex_unlock -- unlock a mutex. <pthread.h>
+  int pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+  If the current thread holds the lock on 'mutex', then the pthread_mutex_unlock()
+  function unlocks 'mutex'. Calling pthread_mutex_unlock() with a 'mutex' that
+  the calling thread does not hold will result in undefined behavior.
+  If successful, pthread_mutex_unlock() function will return zero, otherwise an error
+  number will be returned to indicate the error.
+   */
+  pthread_mutex_unlock(&beers_lock);
+  printf("beers = %i\n", beers);
   return NULL;
 }
 
